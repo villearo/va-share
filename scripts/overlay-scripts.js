2e5bf96599ba7d439089f0ed441b831587e21fdb
Ville Aro <ville_aro@icloud.com>
2018-03-08 17:06:58 +0100

    made some performance improvements

--- scripts/overlay-scripts.js
+++ scripts/overlay-scripts.js
@@ -1,23 +1,6 @@
-// Set global vars
-var vaOverlayElement;
-
-window.addEventListener('load', vaOverlay);
-
-function vaOverlay() {
-	vaOverlayElement = document.getElementById( 'va-overlay' );
-    var buttonOpen = document.getElementById( 'va-overlay-open' );
-    var buttonClose = document.getElementById( 'va-overlay-close' );
-    buttonOpen.addEventListener('click', function() {
-    	toggleOverlayVisibility();
-    });
-	buttonClose.addEventListener('click', function() {
-    	toggleOverlayVisibility();
-    });
-
-}
-
-// Show and hide overlay (this function is marked at admin options page and can be called globally)
+document.getElementById('va-overlay-open').addEventListener('click', toggleOverlayVisibility, false);
+document.getElementById('va-overlay-close').addEventListener('click', toggleOverlayVisibility, false);
 function toggleOverlayVisibility() {
-	vaOverlayElement.classList.toggle( 'visible' );
-	document.body.classList.toggle( 'no-scroll' );
+	document.getElementById('va-overlay').classList.toggle('visible');
+	document.body.classList.toggle('no-scroll');
 }
